18da1f941e8e38b4e52ac75e6612bae9af91cd85
bcooper94 <bcoopercs@live.com>
2015-04-23 00:35:14 -0700

    Fixed operation mappings for all immediate type instructions.

--- Assembler/src/Operation.java
+++ Assembler/src/Operation.java
@@ -11,15 +11,15 @@ public enum Operation {
     SRA(0x03, InstructType.REGISTER),
     SUB(0x22, InstructType.REGISTER),
     SLTU(0x2b, InstructType.REGISTER),
-    ADDIU(0x90000000, InstructType.IMMEDIATE),
-    SLTIU(0xb0000000, InstructType.IMMEDIATE),
-    BEQ(0x40000000, InstructType.IMMEDIATE),
-    BNE(0x50000000, InstructType.IMMEDIATE),
-    LW(0x23000000, InstructType.IMMEDIATE),
-    SW(0x2b000000, InstructType.IMMEDIATE),
-    J(0x20000000, InstructType.JUMP),
+    ADDIU(0x24000000, InstructType.IMMEDIATE),
+    SLTIU(0x2C000000, InstructType.IMMEDIATE),
+    BEQ(0x10000000, InstructType.IMMEDIATE),
+    BNE(0x14000000, InstructType.IMMEDIATE),
+    LW(0x8C000000, InstructType.IMMEDIATE),
+    SW(0xAC000000, InstructType.IMMEDIATE),
+    J(0x08000000, InstructType.JUMP),
     JR(0x08, InstructType.REGISTER),
-    JAL(0x20000000, InstructType.JUMP);
+    JAL(0x0C000000, InstructType.JUMP);
     
     private final int opValue;
     private final InstructType type;

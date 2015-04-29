86ddc1cbde804d1ac0246bbeb9e1ac9e6851a880
dbarraca <dbarraca@calpoyly.edu>
2015-04-29 05:23:29 -0700

    remove prints in Instruction.java and implement simulator.java  methods

--- Assembler/src/Simulator.java
+++ Assembler/src/Simulator.java
@@ -1,5 +1,8 @@
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Arrays;
+import java.io.IOException;
+//import java.io.Integer;
 
 /**
  * A MIPS simulator.
@@ -10,6 +13,7 @@ public class Simulator {
     private int PC;
     private int[] registers;
     private int[] memory;
+    private int endOfText;
     private int cycleCount;
     private int instructCount;
     private int memoryRefs;
@@ -18,9 +22,22 @@ public class Simulator {
         PC = PC_START;
         registers = new int[32];
         memory = new int[MEM_SIZE];
+        endOfText = PC_START;
         cycleCount = 0;
         instructCount = 0;
         memoryRefs = 0;
+    }
+    
+    
+    private byte decToHex(int dec) {
+        byte hex;
+        if (dec >= 48 && dec <= 57) {
+            hex = (byte)(dec - 48);
+        }
+        else {
+            hex = (byte)(dec - 87);
+        }
+        return hex;
     }
     
     /**
@@ -28,7 +45,26 @@ public class Simulator {
      * @param input
      */
     public void loadProgram(InputStream input) {
-        
+        int dec;
+        byte[] bytes = new byte[8];
+        try{
+            //fill memory with opcodes
+            for(int address = PC_START; input.available() != 0; address++) {
+          
+                //find an opcode from input
+                for(int ndx = 0; ndx < 8; ndx++) {
+                    dec = input.read();
+                    bytes[ndx] = decToHex(dec);
+                }
+
+                memory[address] = byteArrToInt(bytes);
+                endOfText = address;
+           //   System.out.println(Integer.toBinaryString(byteArrToInt(bytes)));
+            }
+        }
+        catch(IOException except) {
+           System.err.println("Unable to write buffer.");
+        }
     }
     
     /**
@@ -37,7 +73,7 @@ public class Simulator {
      * @return Contents at <code>address.</code>
      */
     private int accessMemory(int address) {
-        return 0;
+        return memory[address];
     }
     
     /**
@@ -45,7 +81,11 @@ public class Simulator {
      * @param instructCode
      */
     private boolean executeNextInstruct() {
-        return false;
+        int currPC = PC;
+        PC++;
+        System.out.println(Integer.toBinaryString(accessMemory(currPC)));
+        //Operation op = new Operation(accessMemory(currPC) & 0x1F/*, registers*/);
+        return PC <= endOfText;
     }
     
     /**
@@ -53,32 +93,42 @@ public class Simulator {
      * @return
      */
     public boolean singleStep() {
-        return false;
+       executeNextInstruct();
+       registerDump();
+       return false;
     }
     
     /**
      * Run the rest of the program and print contents of every register upon completion.
      */
     public void run() {
-        
+        boolean hasNext = true;
+        while(registers[2] != 10 && hasNext) {
+            hasNext = executeNextInstruct();
+        }
+        registerDump();
     }
     
     /**
      * Print the contents of every register.
      */
     public void registerDump() {
-        
+        System.out.println("\n" + Arrays.toString(registers));
     }
     
     /**
      * Convert a byte aray (little endian) to an int.
      */
     public static int byteArrToInt(byte[] bytes) {
-        int newInt = bytes[0];
+        int newInt = bytes[7];
         
-        newInt |= ((int)bytes[1]) << 8;
-        newInt |= ((int)bytes[2]) << 16;
-        newInt |= ((int)bytes[3]) << 24;
+        newInt |= ((int)bytes[6]) << 4;
+        newInt |= ((int)bytes[5]) << 8;
+        newInt |= ((int)bytes[4]) << 12;
+        newInt |= ((int)bytes[3]) << 16;
+        newInt |= ((int)bytes[2]) << 20;
+        newInt |= ((int)bytes[1]) << 24;
+        newInt |= ((int)bytes[0]) << 28;
         
         return newInt;
     }

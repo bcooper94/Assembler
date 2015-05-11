57a7380752cbc228ba8f259406b942be4014350a
bcooper94 <bcoopercs@live.com>
2015-05-11 11:29:49 -0700

    Created Cycle interface and MultiCycle enum

--- Assembler/src/Operation.java
+++ Assembler/src/Operation.java
@@ -4,43 +4,43 @@ import java.util.HashMap;
  * Represent each supported operation with its func/op code and instruction mode.
  */
 public enum Operation {
-    AND(0x24, InstructType.REGISTER, 4) {
+    AND(0x24, InstructType.REGISTER, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs) {
             regs[getRDReg(instructCode)] = regs[getRSReg(instructCode)] & regs[getRTReg(instructCode)];
         }
     },
     
-    OR(0x25, InstructType.REGISTER, 4) {
+    OR(0x25, InstructType.REGISTER, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs) {
             regs[getRDReg(instructCode)] = regs[getRSReg(instructCode)] | regs[getRTReg(instructCode)];
         }
     },
     
-    ORI(0x34000000, InstructType.IMMEDIATE, 4) {
+    ORI(0x34000000, InstructType.IMMEDIATE, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs, int[] memory) {
             regs[getRTReg(instructCode)] = regs[getRSReg(instructCode)] | getImmediate(instructCode);
         }
     },
     
-    ADD(0x20, InstructType.REGISTER, 4) {
+    ADD(0x20, InstructType.REGISTER, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs) {
             regs[getRDReg(instructCode)] = regs[getRSReg(instructCode)] + regs[getRTReg(instructCode)];
         }
     },
     
-    ADDU(0x21, InstructType.REGISTER, 4) {
+    ADDU(0x21, InstructType.REGISTER, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs) {
 
         }
     },
     
-    SLL(0x00, InstructType.REGISTER, 4) {
+    SLL(0x00, InstructType.REGISTER, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs) {
             regs[getRDReg(instructCode)] = regs[getRTReg(instructCode)] << getShamt(instructCode);
         }
     },
     
-    SRL(0x02, InstructType.REGISTER, 4) {
+    SRL(0x02, InstructType.REGISTER, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs) {
             int MASK = 0x80000000;
             
@@ -52,19 +52,19 @@ public enum Operation {
         }
     },
     
-    SRA(0x03, InstructType.REGISTER, 4) {
+    SRA(0x03, InstructType.REGISTER, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs) {
             regs[getRDReg(instructCode)] = regs[getRTReg(instructCode)] >> getShamt(instructCode);
         }
     },
     
-    SUB(0x22, InstructType.REGISTER, 4) {
+    SUB(0x22, InstructType.REGISTER, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs) {
             regs[getRDReg(instructCode)] = regs[getRSReg(instructCode)] - regs[getRTReg(instructCode)];
         }
     },
     
-    SLTU(0x2b, InstructType.REGISTER, 4) {
+    SLTU(0x2b, InstructType.REGISTER, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs) {
             long rs = Integer.toUnsignedLong(regs[getRSReg(instructCode)]);
             long rt = Integer.toUnsignedLong(regs[getRTReg(instructCode)]);
@@ -73,13 +73,13 @@ public enum Operation {
         }
     },
     
-    ADDI(0x20000000, InstructType.IMMEDIATE, 4) {
+    ADDI(0x20000000, InstructType.IMMEDIATE, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs, int[] memory) {
             regs[getRTReg(instructCode)] = regs[getRSReg(instructCode)] + signExtendImmediate(getImmediate(instructCode));
         }
     },
     
-    ADDIU(0x24000000, InstructType.IMMEDIATE, 4) {
+    ADDIU(0x24000000, InstructType.IMMEDIATE, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs, int[] memory) {
             long rs = Integer.toUnsignedLong(regs[getRSReg(instructCode)]);
             long immediate = Integer.toUnsignedLong(signExtendImmediate(getImmediate(instructCode)));
@@ -88,7 +88,7 @@ public enum Operation {
         }
     },
     
-    SLTIU(0x2C000000, InstructType.IMMEDIATE, 4) {
+    SLTIU(0x2C000000, InstructType.IMMEDIATE, MultiCycle.REGISTER) {
         public void apply(int instructCode, int[] regs, int[] memory) {
             long rs = Integer.toUnsignedLong(regs[getRSReg(instructCode)]);
             long immediate = Integer.toUnsignedLong(getImmediate(instructCode));
@@ -97,20 +97,20 @@ public enum Operation {
         }
     },
     
-    BEQ(0x10000000, InstructType.IMMEDIATE, 4) {
+    BEQ(0x10000000, InstructType.IMMEDIATE, MultiCycle.BRANCH) {
         public int apply(int instructCode, int[] regs, int[] memory, int pc, Simulator sim) {
             int newPC = pc;
             if (regs[getRSReg(instructCode)] == regs[getRTReg(instructCode)]) {
                 newPC += getImmediate(instructCode);
             }
             else {
-//                sim.branchNotTaken();
+                sim.branchNotTaken();
             }
             return newPC;
         }
     },
     
-    BNE(0x14000000, InstructType.IMMEDIATE, 4) {
+    BNE(0x14000000, InstructType.IMMEDIATE, MultiCycle.BRANCH) {
         public int apply(int instructCode, int[] regs, int[] memory, int pc, Simulator sim) {
             int newPC = pc;
             
@@ -118,50 +118,50 @@ public enum Operation {
                 newPC += signExtendImmediate((getImmediate(instructCode)));
             }
             else {
-//                sim.branchNotTaken();
+                sim.branchNotTaken();
             }
             return newPC;
         }
     },
     
-    LW(0x8C000000, InstructType.IMMEDIATE, 5) {
+    LW(0x8C000000, InstructType.IMMEDIATE, MultiCycle.LOAD) {
         public void apply(int instructCode, int[] regs, int[] memory) {
             regs[getRTReg(instructCode)] = memory[(getImmediate(instructCode)) + regs[getRSReg(instructCode)]];
         }
     },
     
-    LUI(0x3C000000, InstructType.IMMEDIATE, 4) {
+    LUI(0x3C000000, InstructType.IMMEDIATE, MultiCycle.LOAD) {
         public void apply(int instructCode, int[] regs, int[] memory) {
             regs[getRTReg(instructCode)] = getImmediate(instructCode) << 16;
         }
     },
     
-    SW(0xAC000000, InstructType.IMMEDIATE, 4) {
+    SW(0xAC000000, InstructType.IMMEDIATE, MultiCycle.STORE) {
         public void apply(int instructCode, int[] regs, int[] memory) {
             memory[(getImmediate(instructCode)) + regs[getRSReg(instructCode)]] = regs[getRTReg(instructCode)];
         }
     },
     
-    J(0x08000000, InstructType.JUMP, 3) {
+    J(0x08000000, InstructType.JUMP, MultiCycle.BRANCH) {
         public int apply(int instructCode, int[] regs, int[] memory, int pc) {
             return signExtendJump(instructCode & 0x03FFFFFF);
         }
     },
     
-    JR(0x08, InstructType.REGISTER, 3) {
+    JR(0x08, InstructType.REGISTER, MultiCycle.BRANCH) {
         public int apply(int instructCode, int[] regs, int[] memory, int pc) {
             return regs[getRSReg(instructCode)];
         }
     },
     
-    JAL(0x0C000000, InstructType.JUMP, 3) {
+    JAL(0x0C000000, InstructType.JUMP, MultiCycle.BRANCH) {
         public int apply(int instructCode, int[] regs, int[] memory, int pc) {
             regs[31] = pc;
             return Simulator.PC_START + signExtendJump(instructCode & 0x03FFFFFF);
         }
     },
     
-    SYSCALL(0xFC000000, InstructType.REGISTER, 0) {
+    SYSCALL(0xFC000000, InstructType.REGISTER, MultiCycle.SYSCALL) {
         public void apply(int instructCode, int[] regs) {
             throw new RuntimeException("Finished running");
         }
@@ -175,7 +175,7 @@ public enum Operation {
     private static final int FUNC_MASK = 0x0000003F;
     private final int opValue;
     private final InstructType type;
-    private final int cycles;
+    private final MultiCycle cycleType;
     private static HashMap<Integer, Operation> opMap;
     
     static {
@@ -186,10 +186,10 @@ public enum Operation {
         }
     }
     
-    private Operation(int opValue, InstructType type, int cycles) {
+    private Operation(int opValue, InstructType type, MultiCycle cycleType) {
         this.opValue = opValue;
         this.type = type;
-        this.cycles = cycles;
+        this.cycleType = cycleType;
     }
     
     /**
@@ -218,7 +218,7 @@ public enum Operation {
      * Get the cycles per instruction of an instruction.
      */
     public int getCyclesPerInstruct() {
-        return this.cycles;
+        return cycleType.getCycles();
     }
     
     /**

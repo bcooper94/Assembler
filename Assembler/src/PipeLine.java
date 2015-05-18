33d8405042139085bd8a2feff6a4eba219c3171a
dbarraca <dbarraca@calpoyly.edu>
2015-05-18 12:09:18 -0700

    pipeline is being clear at all jumps and branches

--- Assembler/src/PipeLine.java
+++ Assembler/src/PipeLine.java
@@ -10,44 +10,40 @@ public class PipeLine {
         cycleCount = 0;
     }
     
-    
     public void pipe(int newInstruct) {
-        //chkFinished();
         for (int idx = 3; idx >= 0; idx--) {
-         //  if(instructWrappers[idx + 1] != null) {
-               instructWrappers[idx + 1] = instructWrappers[idx];
-           //}
-          // System.out.print(instructWrappers[idx+1].getInstructCode());
+            instructWrappers[idx + 1] = instructWrappers[idx];
         }
         
         cycleCount++;
         insert(newInstruct);
     }
-    
 
     public void chkFinished() {
-        //if(instructWrappers[2] != null)
-         //   instructWrappers[2].apply();
-    
         if(instructWrappers[4] != null && instructWrappers[4].getCycleType() == MultiCycle.LOAD) {
             instructWrappers[4].apply();
+            //System.out.print(/*sim.getPC()+*/" "+instructWrappers[4].getCycleType()+"\n");
             instructWrappers[4] = null;
         }
         
         if(instructWrappers[3] != null && (instructWrappers[3].getCycleType() == MultiCycle.STORE || 
            instructWrappers[3].getCycleType() == MultiCycle.REGISTER)) {
             instructWrappers[3].apply();
+            //System.out.print(/*sim.getPC()+*/" "+instructWrappers[3].getCycleType()+"\n");
             instructWrappers[3] = null;
         }
 
         if(instructWrappers[2] != null && (instructWrappers[2].getCycleType() == MultiCycle.BRANCH || 
            instructWrappers[2].getCycleType() == MultiCycle.JUMP)) {
             instructWrappers[2].apply();
+            clearPipe();
+           // System.out.print(/*sim.getPC()+*/" "+instructWrappers[2].getCycleType()+"\n");
             instructWrappers[2] = null;
         }
         
         if(instructWrappers[1] != null && instructWrappers[1].getCycleType() == MultiCycle.NOP) {
             instructWrappers[1].apply();
+           // System.out.print(/*sim.getPC()+*/" "+instructWrappers[1].getCycleType()+"\n");
             instructWrappers[1] = null;
         }
     }
@@ -55,11 +51,14 @@ public class PipeLine {
     public void insert(int instructCode) {
         instructWrappers[0] = new InstructWrapper(instructCode);
         instructWrappers[0].setSimulator(sim);
+        
+        //System.out.print(sim.getPC()+" "+*/instructWrappers[0].getCycleType()+"\n");
     }
     
     //At start make sure to have one in so they are not all null
     public boolean isEmpty() {
         boolean isEmpty = true;
+
         for(int idx = 0; idx <= 4; idx++) {
            if(instructWrappers[idx] != null) {
               isEmpty = false;
@@ -71,5 +70,12 @@ public class PipeLine {
     
     public int getCycleCount() {
        return cycleCount;
+    }
+    
+    private void clearPipe() {
+        instructWrappers = new InstructWrapper[5];
+        /*for(int idx = 0; idx < 4; idx++) {
+            instructWrappers[idx] = null;
+        } */
     }
 }
--- Assembler/src/Simulator.java
+++ Assembler/src/Simulator.java
@@ -45,7 +45,6 @@ public class Simulator {
         String immed = "0";
         String labelString;
 
-
         if(dataDef.equals(".word") || dataDef.equals(".byte") ) {
             immed = convertHex(args[2]);
             memory[address] = Integer.parseInt(immed, 10);
@@ -203,10 +202,11 @@ public class Simulator {
      * @param instructCode
      */
     private boolean executeNextInstruct() {
-       // System.out.print(PC);
+        System.out.print(PC+" ");
         int instructCode = memory[PC++];
         pipeLine.pipe(instructCode);
         pipeLine.chkFinished();
+        
         instructCount++;
         
         /*
@@ -230,11 +230,11 @@ public class Simulator {
         else {
             PC = op.apply(instructCode, registers, memory, PC);
         }
-        
-        instructCount++;
-        
-        cycleCount += op.getCyclesPerInstruct();
         */
+        //instructCount++;
+        
+        //cycleCount += op.getCyclesPerInstruct();
+        
         return PC <= endOfText;
     }
     
